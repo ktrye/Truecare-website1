c98e274177c0e336f84252c486f68112c482dd06
ktrye <katierye@gmail.com>
2026-06-27 16:59:53 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const menuButton = document.querySelector('.menu-toggle');
-const nav = document.querySelector('.nav');
-if (menuButton && nav) {
-  menuButton.addEventListener('click', () => nav.classList.toggle('open'));
-}
-document.querySelectorAll('.nav a').forEach(link => {
-  link.addEventListener('click', () => nav?.classList.remove('open'));
-});
