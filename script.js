35494a417cee0837851e26d0481fdbaaed26349a
ktrye <katierye@gmail.com>
2026-06-27 18:19:38 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const menuButton = document.querySelector('.menu-toggle');
-const nav = document.querySelector('.nav');
+const menuButton = document.querySelector('.menu-button');
+const nav = document.querySelector('.main-nav');
 if (menuButton && nav) {
   menuButton.addEventListener('click', () => nav.classList.toggle('open'));
 }
-document.querySelectorAll('.nav a').forEach(link => {
+document.querySelectorAll('.main-nav a').forEach(link => {
   link.addEventListener('click', () => nav?.classList.remove('open'));
 });
